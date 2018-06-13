5e519358d33a2aa070ab4089e4d0c54aec2e4f4d
Ryan <rvvergara@gmail.com>
2018-06-13 15:32:06 +0800

    Do Bitcoin price checker exercise

--- Section7-AJAX-Part1-XHR-and-Fetch/assets/js/bitcoin-starter.js
+++ Section7-AJAX-Part1-XHR-and-Fetch/assets/js/bitcoin-starter.js
@@ -1,18 +1,37 @@
 let XHR     =   new XMLHttpRequest(),
     btn     =   document.querySelector("button"),
     title   =   document.querySelector("title"),
-    price   =   document.querySelector("#price");
+    price   =   document.querySelector("#price"),
+    form    =   document.querySelector("form"),
+    currencies = document.querySelectorAll("input[type='radio']"),
+    currency = "",
+    currencySyms = {
+            EUR: "€",
+            USD: "$",
+            GBP: "£",
+        },
+    sym = "";
+
+function selectCurrency(arr){
+    for(let radio of arr){
+        if(radio.checked){
+            currency = radio.id;
+            sym = currencySyms[currency];
+        }
+    }
+    return [currency,sym];
+}
 
 function updatePrice(){
     XHR.onreadystatechange = function(){
         if(XHR.readyState == 4 && XHR.status == 200){
-            let updatedPrice = "$ "+ (JSON.parse(XHR.responseText).bpi.USD.rate);
+            let updatedPrice = selectCurrency(currencies)[1]+" "+ (JSON.parse(XHR.responseText).bpi[selectCurrency(currencies)[0]].rate);
+            updatedPrice = updatedPrice.slice(0,updatedPrice.length-2);
             price.classList.add("animate");
             price.textContent = updatedPrice;
             title.textContent = updatedPrice+" - Bitcoin Price Exercise | AJAX"
         }
     }
-    
     XHR.open("GET","https://api.coindesk.com/v1/bpi/currentprice.json");
     XHR.send();
     price.classList.remove("animate");
@@ -20,5 +39,6 @@ function updatePrice(){
 
 setInterval(updatePrice,30000);
 
+form.addEventListener("change",updatePrice);
 window.addEventListener("load",updatePrice);
 btn.addEventListener("click",updatePrice);
