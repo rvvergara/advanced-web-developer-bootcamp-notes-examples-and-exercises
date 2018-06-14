4f953a438b953032633fa6564e4ef37ca6fd6701
Ryan <rvvergara@gmail.com>
2018-06-14 13:32:07 +0800

    Do random user exercise

---
+++ Section7-AJAX-Part1-XHR-and-Fetch/assets/js/user-generator.js
@@ -0,0 +1,55 @@
+
+let url         =   "https://randomuser.me/api/",
+    btn         =   document.querySelector("#btn"),
+    avatar      =   document.querySelector("#avatar"),
+    fullname    =   document.querySelector("#fullname"),
+    username    =   document.querySelector("#username"),
+    email       =   document.querySelector("#email"),
+    city        =   document.querySelector("#city"),
+    animateColl =   [avatar, fullname, username, email, city];
+
+function generateUser(){
+    fetch(url)
+    .then(handleError)
+    .then(parseResponse)
+    .then(newUSer)
+    .catch(printError)
+    toggleAnimation();
+}
+
+function handleError(response){
+    if(!response) throw Error(response.status);
+    return response;
+}
+
+function parseResponse(response){
+    // parse json
+    return response.json();
+}
+
+function newUSer(response){
+    toggleAnimation();
+    let data = response.results[0];
+    avatar.src  =   data.picture.medium;
+    fullname.textContent = data.name.first+" "+data.name.last;
+    username.textContent = data.login.username;
+    email.textContent   =   data.email;
+    city.textContent    =   data.location.city;
+}
+
+function removeAnimation(){
+    userProfile.classList.remove("animate");
+}
+
+function printError(err){
+    // print error
+    alert(err);
+}
+
+function toggleAnimation(){
+    for(el of animateColl){
+        el.classList.toggle("animate");
+    }
+}
+
+btn.addEventListener("click",generateUser);
