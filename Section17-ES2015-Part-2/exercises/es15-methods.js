97071b075f282ef78b6753d17658c9f0d39449be
Ryan <rvvergara@gmail.com>
2018-06-24 13:19:19 +0800

    Do es2015-methods exercise

---
+++ Section17-ES2015-Part-2/exercises/es15-methods.js
@@ -0,0 +1,60 @@
+/* 
+Write a function called copyObject, which accepts one parameter, an object. The function should return a shallow copy of the object.
+
+var o = {name: 'Elie'}
+var o2 = copyObject({}, o)
+o2.name = "Tim"
+o2.name // 'Tim'
+o.name // 'Elie'
+*/
+
+let copyObject = obj => Object.assign({},obj);
+
+/* 
+
+Write a function called checkIfFinite which accepts one parameter and returns true if that parameter is a finite number.
+
+checkIfFinite(4) // true
+checkIfFinite(-3) // true
+checkIfFinite(4. // .toEqual(true
+checkIfFinite(NaN) // false
+checkIfFinite(Infinity) // false
+*/
+
+let checkIfFinite = num => Number.isFinite(num);
+
+/*
+
+Write a function called areAllNumbersFinite which accepts an array and returns true if every single value in the array is a finite number, otherwise return false.
+
+var finiteNums = [4,-3,2.2]
+var finiteNumsExceptOne = [4,-3,2.2,NaN]
+areAllNumbersFinite(finiteNums) // true
+areAllNumbersFinite(finiteNumsExceptOne) // false
+*/
+
+let areAllNumbersFinite = arr => arr.every(val => Number.isFinite(val));
+
+/* 
+
+Write a function called convertArrayLikeObject which accepts a single parameter, an array like object. The function should return the array like object converted to an array.
+
+var divs = document.getElementsByTagName('div')
+divs.reduce // undefined
+
+var converted = convertArrayLikeObject(divs)
+converted.reduce // funciton(){}...
+*/
+
+let convertArrayLikeObject = arrlike => Array.from(arrlike);
+
+/*
+
+Write a function called displayEvenArguments which accepts a variable number of arguments and returns a new array with all of the arguments that are even numbers.
+
+displayEvenArguments(1,2,3,4,5,6) // [2,4,6]
+displayEvenArguments(7,8,9) // [8]
+displayEvenArguments(1,3,7) // []
+*/
+
+let displayEvenArguments = (...args) => args.filter(val => val%2 === 0);
