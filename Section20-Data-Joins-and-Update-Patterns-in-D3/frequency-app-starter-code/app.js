d14145db9ed7133149e8fd3bf2b52136785bb263
Ryan <rvvergara@gmail.com>
2018-06-27 15:20:57 +0800

    Cleanup code in frequency app

--- Section20-Data-Joins-and-Update-Patterns-in-D3/frequency-app-starter-code/app.js
+++ Section20-Data-Joins-and-Update-Patterns-in-D3/frequency-app-starter-code/app.js
@@ -1,71 +1,43 @@
 // write your code here!
-const   input   =   d3.select("input"),
-        reset   =   d3.select("#reset"),
-        phrase  =   d3.select("#phrase"),
-        count   =   d3.select("#count"),
-        letters =   d3.select("#letters");
-    
-let upData = [];
-
 d3.select("form").on("submit",()=>{
+    let inputText = d3.select("input").property("value").toLowerCase(),
+        enterData = Array.from(new Set(inputText.split("").sort())),
+        updateSelect = d3.select("#letters")
+                         .selectAll("div")
+                         .data(enterData,d=>d),
+        enterSelect = updateSelect.enter();
+
     d3.event.preventDefault();
     
-    let inputText       = input.property("value").toLowerCase(),
-        enterData       = new Set(),
-        dataObj         = freqObj(inputText,enterData),
-        newData         = inputText.split(""),
-        newCharCount    =   0;
-        
-    enterData = Array.from(enterData).sort();
-
-    let updateSelect = d3.select("#letters")
-                          .selectAll("div")
-                          .data(enterData,d=>{
-                            //   if(!upData.includes(d)) return d;
-                            return d.character;
-                          });
     updateSelect
-        .classed("new",false)
-        .exit()
-        .remove();
+      .classed("new",false)
+      .exit()
+      .remove();
 
-    updateSelect
-        .enter()                      
-        .append("div")
-        .text(d=>d)
-        .classed("letter",true)
-        .merge(updateSelect)
-        .style("width","20px")
-        .style("height",d=>`${dataObj[0][d]*20}px`)
-        .style("line-height","20px")
-        .style("margin-right","5px")
-        .classed("new",d=>!upData.includes(d));
+    enterSelect
+      .append("div")
+      .classed("new",true)
+      .text(d=>d)
+    .merge(updateSelect)
+      .classed("letter",true)                   
+      .style("width","20px")
+      .style("height",d=>`${frequency(inputText)[d]*20}px`)
+      .style("line-height","20px")
+      .style("margin-right","5px");  
 
-    phrase.text(`Analysis of: ${inputText}`);
-    count.text(`(New characters: ${dataObj[1]})`);
-    
-    upData = enterData;
-    input.property("value","");
+    d3.select("#phrase").text(`Analysis of: ${inputText}`);
+    d3.select("#count").text(`(New characters: ${enterSelect.nodes().length})`)
+    d3.select("input").property("value","");  
 });
 
-reset.on("click",()=>{
-    phrase.text("");
-    count.text("");
-    input.property("value","");
+d3.select("#reset").on("click",()=>{
+    d3.select("#phrase").text("");
+    d3.select("#count").text("");
     d3.selectAll(".letter").remove();
-    upData = [];
 })
 
-function freqObj(word,set,dataArr){
-    let dataObj = {},
-        newCharCount = 0;
-
-    for(let char of word){
-        dataObj[char]===undefined?dataObj[char]=1:dataObj[char]++;
-        set.add(char);
-    }
-    
-    for(let letter of set) upData.includes(letter)?newCharCount:newCharCount++;
-
-    return [dataObj,newCharCount];
+function frequency(str){
+    let obj = {}
+    for(let letter of str.split("")) obj[letter]===undefined?obj[letter]=1:obj[letter]++;
+    return obj;
 }
