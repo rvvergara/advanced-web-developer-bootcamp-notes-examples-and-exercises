f584a713033cc3d313320bbf0dce6ac7e25ada95
Ryan <rvvergara@gmail.com>
2018-07-27 10:25:46 +0800

    Put final touches on styling on warbler app

--- Section34-37-Warbler-Final-Project/warbler/warbler-client/src/services/api.js
+++ Section34-37-Warbler-Final-Project/warbler/warbler-client/src/services/api.js
@@ -10,7 +10,8 @@ export function setTokenHeader(token){
 
 export function apiCall(method, path, data){
     return new Promise((resolve,reject)=>{
-        return axios[method](path,data).then(res => {
+        return axios[method](path,data)
+        .then(res => {
             return resolve(res.data)
         })
         .catch(err=>{
